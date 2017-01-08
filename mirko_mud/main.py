bbca66fb51e9e4ce3d64784950e1d322953520dd
wytrzeszcz <kuba@witomi.pl>
2017-01-08 20:19:26 +0100

    read only new natyfication

--- mirko_mud/main.py
+++ mirko_mud/main.py
@@ -5,19 +5,20 @@ api.authenticate(auth.login, auth.connection_key)
 notyfication=api.get_notifications();
 anserws={}
 for call in notyfication:
-	print "author"
-	print call['author']
-	print "URL"
-	print call['url']
-	print "id"
-	print call['entry']['id']
-	print 
-	if call['entry']['id'] not  in anserws.keys():
-		anserws[call['entry']['id']]=""
-	anserws[call['entry']['id']]+="Zawolales mnie @"
-	anserws[call['entry']['id']]+=call['author']
-	anserws[call['entry']['id']]+=" tutaj to Ci odpowiadam, przeprasza jednoczesnie za spam\n powinno to byc ostatnie\n"
-	api.mark_as_read_notification(call['id'])
+	if call['new'] :
+		print "author"
+		print call['author']
+		print "URL"
+		print call['url']
+		print "id"
+		print call['entry']['id']
+		print 
+		if call['entry']['id'] not  in anserws.keys():
+			anserws[call['entry']['id']]=""
+		anserws[call['entry']['id']]+="Zawolales mnie @"
+		anserws[call['entry']['id']]+=call['author']
+		anserws[call['entry']['id']]+=" tutaj to Ci odpowiadam, przeprasza jednoczesnie za spam\n powinno to byc ostatnie\n"
+		api.mark_as_read_notification(call['id'])
 
 
 for key in anserws.keys():
