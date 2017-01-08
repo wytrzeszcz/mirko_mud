b8c471fc371b8ce292cee9c2314777e425265bc5
wytrzeszcz <kuba@witomi.pl>
2017-01-08 22:11:06 +0100

    i hope now it works, and give cat for evry call onece

--- mirko_mud/main.py
+++ mirko_mud/main.py
@@ -6,8 +6,12 @@ api.authenticate(auth.login, auth.connection_key)
 
 anserws={}
 while True:
-	notyfication=api.get_notifications();
-
+	try:
+		notyfication=api.get_notifications()
+		api.mark_as_read_notifications()
+	except:
+		print "Sky is a limit, but API has limit too"
+		time.sleep(20)
 	for call in notyfication:
 		if call['new'] :
 			if call['type'] in ["entry_comment_directed","entry_directed","entry_tag"]:
@@ -23,17 +27,18 @@ while True:
 				anserws[call['entry']['id']]+="Zawolales mnie @"
 				anserws[call['entry']['id']]+=call['author']
 				anserws[call['entry']['id']]+=" tutaj to Ci odpowiadam, przeprasza jednoczesnie za spam\n powinno to byc ostatnie\n"
-			api.mark_as_read_notification(call['id'])
-
+		
 
 	for key in anserws.keys():
 		try:
 			print key
 			print anserws[key]	
 			api.add_entry_comment(key,anserws[key],"http://cdn.skim.gs/images/v1/msi/dc1h0q9jtxrqdqpwt6ai/tips-for-raising-a-healthy-and-happy-cat")
-			anserws.pop(key,nil) # after ok anserw delete
+			print "NO EXPECTION"
+			del anserws[key] # after ok anserw delete
+			print anserws
 		except:
 			print "Sky is a limit, but API has limit too"
-			time.sleep(120) 		
+			time.sleep(20) 		
 
 
