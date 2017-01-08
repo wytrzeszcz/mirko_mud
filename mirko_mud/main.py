0a4dbb911c832b5669c58490e0fddb5209ac3b93
wytrzeszcz <kuba@witomi.pl>
2017-01-08 18:10:19 +0100

    try to anserw all calls

--- mirko_mud/main.py
+++ mirko_mud/main.py
@@ -1,5 +1,23 @@
 import wykop
 import auth 
-api.authenticate(login, connection_key)
-api.add_entry("czesc @wytzzeszcz i ekipo od #mudnamirko, sprawdzam czy moge mowic")
+api=wykop.WykopAPI(auth.appkey,auth.secretkey)
+api.authenticate(auth.login, auth.connection_key)
+notyfication=api.get_notifications();
+anserws={}
+for call in notyfication:
+	print "author"
+	print call['author']
+	print "URL"
+	print call['url']
+	print "id"
+	print call['entry']['id']
+	anserws[call['entry']['id']]+="Zawolales mnie @"
+	anserws[call['entry']['id']]+=call['author']
+	anserws[call['entry']['id']]+=" turaj to Ci odpowiadam, przeprasza jednoczesnie za spam\n"
+
+for entry,anserw in anserws:
+	print anserw
+	print entry 
+	
+
 
