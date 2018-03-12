cf980d78c18fd81ad00d073ca6845bd12e8ee5e5
Jakub Staniszewski <wytrzeszcz@NFS.witominska.localnet>
2018-03-13 00:11:05 +0100

    Podstawowa funcjonalnosc,teraz trzeba dodać karty

--- mirko_mud/main.py
+++ mirko_mud/main.py
@@ -1,44 +1,38 @@
 import time
 import wykop
 import auth 
-api=wykop.WykopAPI(auth.appkey,auth.secretkey)
-api.authenticate(auth.login, auth.connection_key)
+import sys
 
+init_done = False 
+while not init_done:
+    try:
+        api=wykop.WykopAPI(auth.appkey,auth.secretkey)
+        api.authenticate(auth.login, auth.connection_key)
+        init_done=True
+    except:
+        print "no auth???"
+        print sys.exc_info()[0]
+        time.sleep(120)
+    
 anserws={}
+id_of_last_post=None;
 while True:
-	try:
-		notyfication=api.get_notifications()
-		api.mark_as_read_notifications()
-	except:
-		print "Sky is a limit, but API has limit too"
-		time.sleep(20)
-	for call in notyfication:
-		if call['new'] :
-			if call['type'] in ["entry_comment_directed","entry_directed","entry_tag"]:
-				print "author"
-				print call['author']
-				print "URL"
-				print call['url']
-				print "id"
-				print call['entry']['id']
-				print 
-				if call['entry']['id'] not  in anserws.keys():
-					anserws[call['entry']['id']]=""
-				anserws[call['entry']['id']]+="Zawolales mnie @"
-				anserws[call['entry']['id']]+=call['author']
-				anserws[call['entry']['id']]+=" tutaj to Ci odpowiadam, przeprasza jednoczesnie za spam\n powinno to byc ostatnie\n"
-		
+    try:
+        if id_of_last_post is not None:
+            print id_of_last_post
+            voters= api.get_entry(id_of_last_post)["voters"]
+            respond="rozliczenie tury:"
+            for vote in voters:
+                print "@"+vote["author"]
+                respond+="\n"
+                respond+="@"+vote["author"]+" :UMAR! bo nie ma tu jeszcze zadnej logiki, ale wiem ze zaplusowane dziekuje"
+            api.add_entry_comment(id_of_last_post,respond)
 
-	for key in anserws.keys():
-		try:
-			print key
-			print anserws[key]	
-			api.add_entry_comment(key,anserws[key],"http://cdn.skim.gs/images/v1/msi/dc1h0q9jtxrqdqpwt6ai/tips-for-raising-a-healthy-and-happy-cat")
-			print "NO EXPECTION"
-			del anserws[key] # after ok anserw delete
-			print anserws
-		except:
-			print "Sky is a limit, but API has limit too"
-			time.sleep(20) 		
-
+        tekst="Jestem bote, i nie bede tagowal bo zaraz nocna a @wytrzzeszcz sie denerwuje, w koncu on mnie pisze, jesli dobrze mnie zaprogramuje powinienem co 5 minut wrzucac ten wpis i wolac plusujacych porzedni do tego co zaplusowali, dzieluje za plusy one pomagaja wytrzowi pisac,elo z farszem"    
+        id_of_last_post=api.add_entry(body=tekst)["id"] # emged=
+    except:
+            print "Sky is a limit, but API has limit too2"
+            print sys.exc_info()[0]
+            time.sleep(210) 		
+    time.sleep(5*60);
 
